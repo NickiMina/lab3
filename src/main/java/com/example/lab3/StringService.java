34de1ba1a47436445c1a3abb217e5524299c679a
NickiMina <olkasia@poczta.one.pl>
2021-11-18 14:11:11 +0100

    utworzenie serwisu ktory komunikuje sie z api zadania 2

---
+++ src/main/java/com/example/lab3/StringService.java
@@ -0,0 +1,18 @@
+package com.example.lab3;
+
+import org.springframework.web.client.RestTemplate;
+
+public class StringService {
+    public ParserFromString parser;
+
+    public ParserFromString getAnswer(String args){
+        final String uri = "http://localhost:8080/checkString/"+args;
+
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+        parser = new ParserFromString(result);
+        parser.setAllVariables(result);
+        return parser;
+    }
+
+}
